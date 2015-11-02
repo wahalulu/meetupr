d8608985dd5f776301bce86bea7e17f890365fb0
Marck Vaisman <marck@vaisman.us>
2015-11-01 20:28:51 -0500

    added export parameter to make this function availble

--- R/extract_result_dataframe.R
+++ R/extract_result_dataframe.R
@@ -1,6 +1,7 @@
 #' Extracts and flattens/unnests a data.frame from a JSON response
 #' @param l A list of JSON responses of the same kind
 #' @return A dataframe
+#' @export
 extract_result_dataframe <- function(l) {
   if(!is.list(l)) stop("Object must be a list of responses!")
   df_list <- lapply(l, function(x) {
